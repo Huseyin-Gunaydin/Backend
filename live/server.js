2609b78151342d52bd3f2ef8656d6d4b9c4c6a03
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2022-11-28 14:13:44 +0100

    const server erstellt

--- live/server.js
+++ live/server.js
@@ -1,1 +1,5 @@
 const http = require("http");
+console.log("starting server...");
+const server = http.createServer((request, response)=>{
+    console.log("getting a request!", request.method, request.url);
+})
