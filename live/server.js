d892136c85cb9b82b5e1896a96dfd3bcb200f4ab
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2022-11-28 14:15:03 +0100

    switch case

--- live/server.js
+++ live/server.js
@@ -2,4 +2,12 @@ const http = require("http");
 console.log("starting server...");
 const server = http.createServer((request, response)=>{
     console.log("getting a request!", request.method, request.url);
-})
+});
+
+switch (request.url){
+    case "/photos":{
+        const photos = [
+            
+        ]
+    }
+}
